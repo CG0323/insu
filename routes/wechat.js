0e8e2e256c9d3446a03107d9632ec9ac738bcb27
cg <wangchun0323@qq.com>
2015-12-31 21:44:50 +0800

    wechat auth use package

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var wechat = require('wechat-enterprise');
 
-/* GET home page. */
-router.get('/auth', function(req, res, next) {
-    console.log(req);
-});
+var config = {
+  token: 'H4MbzV5LAd3n',
+  encodingAESKey: 'T1KvARk5KdSAdJ2ERidwVaC2mSmU5z80GUMjyq5HGnw',
+  corpId: 'wxb9a60da724795b2e'
+};
 
+app.use('/auth', wechat(config, function (req, res, next) {
+  res.writeHead(200);
+  res.end('hello node api');
+}));
 module.exports = router;
