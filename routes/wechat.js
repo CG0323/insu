7c36c06049b2ea29b8842666bac061e86b520b28
cg <wangchun0323@qq.com>
2016-01-08 21:34:12 +0800

    add wechat auth change token

--- routes/wechat.js
+++ routes/wechat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var wechat = require('wechat');
 
 var config = {
-  token: 'H4MbzV5LAd3n',
+  token: 'H4MbzV5LAd3',
   appid: 'wxd168d39b1572120f',
   encodingAESKey: 'T1KvARk5KdSAdJ2ERidwVaC2mSmU5z80GUMjyq5HGnw',
   //corpId: 'wxb9a60da724795b2e'
