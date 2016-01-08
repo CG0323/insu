6f17c63eae2dd87dbbda13d9ad78140ad9183372
cg <wangchun0323@qq.com>
2016-01-08 21:32:18 +0800

    add wechat auth

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var wechat = require('wechat-enterprise');
+var wechat = require('wechat');
 
 var config = {
   token: 'H4MbzV5LAd3n',
+  appid: 'wxd168d39b1572120f',
   encodingAESKey: 'T1KvARk5KdSAdJ2ERidwVaC2mSmU5z80GUMjyq5HGnw',
-  corpId: 'wxb9a60da724795b2e'
+  //corpId: 'wxb9a60da724795b2e'
 };
 
 router.get('/auth', wechat(config, function (req, res, next) {
   res.writeHead(200);
   res.end('hello node api');
 }));
+
+
+
 module.exports = router;
