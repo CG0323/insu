27ece779d3537ac76b075f40c483f743d43d28ee
cg <wangchun0323@qq.com>
2016-01-31 17:49:21 +0600

    自动大写车牌号字母

--- public/app/policy/constrollers/PolicyEditorCtrl.js
+++ public/app/policy/constrollers/PolicyEditorCtrl.js
@@ -86,3 +86,22 @@ angular.module('app.policy').controller('PolicyEditorController', function ($sco
 
     };
 }); 
+
+angular.module('app.policy').directive('upper', function() {
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
+           if(inputValue == undefined) inputValue = '';
+           var capitalized = inputValue.toUpperCase();
+           if(capitalized !== inputValue) {
+              modelCtrl.$setViewValue(capitalized);
+              modelCtrl.$render();
+            }         
+            return capitalized;
+         }
+         modelCtrl.$parsers.push(capitalize);
+         capitalize(scope[attrs.ngModel]);  // capitalize initial value
+     }
+   };
+});
