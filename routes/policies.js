45d3a4c419d4d7ae3b46e55470f24b938cf4ff95
cg <wangchun0323@qq.com>
2016-03-23 21:36:23 +0800

    excel export

--- routes/policies.js
+++ routes/policies.js
@@ -158,7 +158,6 @@ function sendCSV(policies, res){
         console.log(policies.length);
         for (var i = 0; i < policies.length; i++) {
           console.log(i);
-          console.log(policies[i]);
           var policy = policies[i];
           var row = {};
           row.company = {};
@@ -172,24 +171,28 @@ function sendCSV(policies, res){
             row.applicant.name = policy.applicant.name;
             row.plate_no = policy.plate_no;
             row.applicant.phone = "'" + policy.applicant.phone;
+            console.log("A");
             row.organization.name = policy.organization.name;
             row.seller.name = policy.seller.name;
             row.client.name = policy.client.name;
             row.mandatory_fee=policy.mandatory_fee;
             row.mandatory_fee_income=policy.mandatory_fee_income;
             row.mandatory_fee_payment=policy.mandatory_fee_payment;
+            console.log("B");
             row.commercial_fee=policy.commercial_fee;
             row.commercial_fee_income=policy.commercial_fee_income;
             row.commercial_fee_payment=policy.commercial_fee_payment;
             row.tax_fee=policy.tax_fee;
             row.tax_fee_income=policy.tax_fee_income;
             row.tax_fee_payment=policy.tax_fee_payment;
+            console.log("C");
             row.payment_addition = policy.payment_addition? policy.payment_addition : 0;
             row.payment_substraction = policy.payment_substraction? policy.payment_substraction : 0;
             row.total_income=policy.total_income;
             row.total_payment=policy.total_payment;
             row.paid_at= policy.paid_at ? (dateFormat(policy.paid_at, "mm/dd/yyyy")) : '';
             row.payment_bank =policy.payment_bank ? policy.payment_bank : '';
+            console.log("D");
           arr.push(row); 
           console.log("row added");
         }
